258fdcb1922e61fd4c200bd0dd97b6fe07a63daf
omendez30 <omendez9@outlook.com>
2019-01-24 22:36:18 -0500

    created instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,19 @@ class Person {
     console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
   }
 }
+class Instructor extends Person {
+  constructor(attrs) {
+    super(attrs);
+    this.specialty = attrs.specialty;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning ${subject}`);
+  }
+
+  grade(student, subject) {
+    console.log(`${this.name} recieves a perfect score on ${subject}`);
+  }
+}
